2c89cb7be6d5278d836480e70dc8fdf8fd577a29
s567126 <156241739+s567126@users.noreply.github.com>
2024-04-05 14:53:59 -0500

    done

--- controllers/fish.js
+++ controllers/fish.js
@@ -47,7 +47,7 @@ exports.fish_view_all_Page = async function (req, res) {
 
 
 // Handle fish create on POST.
-    exports.fish_create_post = async function (req, res) {
+exports.fish_create_post = async function (req, res) {
     console.log(req.body)
     let document = new fish();
     // We are looking for a body, since POST does not have query parameters.
@@ -66,7 +66,32 @@ exports.fish_view_all_Page = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
-
-
-
-
+// for a specific.fish.
+// for a specific fish.
+exports.fish_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await fish.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
+// Handle fish update form on PUT.
+exports.fish_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await fish.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.name)toUpdate.name = req.body.name;
+        if (req.body.life_span) toUpdate.life_span = req.body.life_span;
+        if (req.body.color) toUpdate.color = req.body.color;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}failed`);
+    }
+};
--- routes/fish.js
+++ routes/fish.js
@@ -3,5 +3,9 @@ const fish_controlers= require('../controllers/fish');
 var router = express.Router();
 /* GET fishs */
 router.get('/', fish_controlers.fish_view_all_Page );
+
+router.get('/fish/:id', fish_controlers.fish_detail);
+
 module.exports = router;
+
 

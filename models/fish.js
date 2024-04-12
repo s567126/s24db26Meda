1fd1c536beba933840be02bb628f1bc21fe7b2c2
s567126 <156241739+s567126@users.noreply.github.com>
2024-04-12 15:53:30 -0500

    done

--- models/fish.js
+++ models/fish.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose")
 const fishSchema = mongoose.Schema({
-name: String,
-life_span: Number,
-color: String
+    name: String,
+    life_span: {
+        type: Number,
+        min: 7,
+        max: 20,
+    },
+    color: String
 })
-module.exports = mongoose.model("fish",fishSchema)
+module.exports = mongoose.model("fish", fishSchema)
--- routes/fish.js
+++ routes/fish.js
@@ -1,25 +1,33 @@
 var express = require('express');
-const fish_controlers= require('../controllers/fish');
+var passport = require('passport');
+const fish_controlers = require('../controllers/fish');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    res.redirect("/login");
+}
 /* GET fishs */
-router.get('/', fish_controlers.fish_view_all_Page );
+router.get('/', fish_controlers.fish_view_all_Page);
 
 router.get('/fish/:id', fish_controlers.fish_detail);
-
 
 /* GET detail fish page */
 router.get('/detail', fish_controlers.fish_view_one_Page);
 
 /* GET create fish page */
-router.get('/create', fish_controlers.fish_create_Page);
+router.get('/create', secured, fish_controlers.fish_create_Page);
 
-/* GET create update page */
-router.get('/update', fish_controlers.fish_update_Page);
+/* GET update fish page */
+router.get('/update', secured, fish_controlers.fish_update_Page);
 
+router.post('/login', passport.authenticate('local'), function (req, res) {
+    res.redirect('/');
+});
 /* GET delete fish page */
-router.get('/delete', fish_controlers.fish_delete_Page);
-
+router.get('/delete', secured, fish_controlers.fish_delete_Page);
 
 module.exports = router;
-
-

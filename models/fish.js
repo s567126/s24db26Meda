ec9b11f17efcd801c6dcd73790f7fff8230b8e95
s567126 <156241739+s567126@users.noreply.github.com>
2024-04-15 17:05:41 -0500

    done

--- models/fish.js
+++ models/fish.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose")
 const fishSchema = mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        minlength: 7,
+        maxlength: 20,
+    },
     life_span: {
         type: Number,
         min: 7,
         max: 20,
     },
+    
     color: String
 })
 module.exports = mongoose.model("fish", fishSchema)
